bf4979fc3ea8a81ef9784853deecf3497a783d32
MrR4ts56 <karit.rerm@bumail.net>
2022-02-19 10:09:21 +0700

    Merge remote-tracking branch 'class-origin/class/06/ex01' into class/06/ex01

---
+++ src/FriendList.js
@@ -0,0 +1,10 @@
+
+
+function FriendList()
+{
+    return(
+        <div>This is my friend list:Doggo Ratta Catty Piggy
+        </div>
+    )
+}
+export default FriendList;
--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Profile from './Profile.js';
-
+import FriendList from './FriendList.js';
 
 function Router()
 {
@@ -10,6 +10,7 @@ function Router()
     <Routes>
         <Route path='/' element={<App/>} />
         <Route path='/profile' element={<Profile/>} />
+        <Route path='/friendlist' element={<FriendList/>} />
     </Routes>
     </BrowserRouter>
     )
