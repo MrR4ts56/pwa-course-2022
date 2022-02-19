3ebe012f3d43263b2c87f7c7be70b1a5563ff520
MrR4ts56 <karit.rerm@bumail.net>
2022-02-19 10:35:35 +0700

    1630706404 Hooray!

--- src/FriendList.js
+++ src/FriendList.js
@@ -1,9 +1,14 @@
-
+import './FriendList.css';
 
 function FriendList()
 {
     return(
-        <div>This is my friend list:Doggo Ratta Catty Piggy
+        <div>
+            <div className='list'>This is my friend list:</div>
+            <div className='list'>Doggo</div>
+            <div className='list'>Kitty</div>
+            <div className='list'>Ratto</div>
+            <div className='list'>Piggy</div>
         </div>
     )
 }
